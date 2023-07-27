ccac6d5944b2cbfbe11495cf145e6f816ed9d6a9
Michael Lay <99146295+michaelnlay@users.noreply.github.com>
2023-07-27 16:57:39 -0400

    Add files via upload

---
+++ Modern_JS/05-loops-iteration-array/01-for-loop/script.js
@@ -0,0 +1,33 @@
+// for (let i=0; i <=10; i++){
+
+//     if (i===7){
+//         console.log('7 is my lucky number');
+//     }else {
+//         console.log('Number ' + i);
+//     }
+
+
+// }
+
+// Nest loops
+
+// for (let i = 1; i <= 10; i++) {
+//     for (let j = 1; j <= 10; j++) {
+// console.log(` ${i} * ${j} = ${i*j}`);
+// //return 1* 1 = 1, 1*2 = 2; 1*3=3....2*1 =2; 2*2=4...
+//     }
+// }
+
+
+//Loop through an array
+const names = ['Brad', 'Sam', 'Sara', 'John', 'Tim'];
+
+for (let i = 0; i < names.length; i++) {
+    if (names[i] === 'Sara') {
+        console.log(names[i] + ' is the best');
+    }
+    else {
+        console.log(names[i]);
+    }
+
+}
