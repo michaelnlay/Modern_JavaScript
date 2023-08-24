38e0736f65c28f36daa2f178ff3ce1339eeb305e
Michael Lay <99146295+michaelnlay@users.noreply.github.com>
2023-08-23 23:18:12 -0400

    Add files via upload

--- Modern_JS/05-loops-iteration-array/08.array.filter/script.js
+++ Modern_JS/05-loops-iteration-array/08.array.filter/script.js
@@ -16,8 +16,39 @@ const numbers = [111111, 2, 3, 4, 6, 11, 2, 333];
 //shorter version
 // const evenNumbers = numbers.filter(number => number % 2 !==0)
 
-
+//Same with forEach
+// let evenNumbers = [];
+// numbers.forEach((number) => {
+//   if (number % 2 === 0) {
+//     evenNumbers.push(number);
+//   }
+// });
 //Try it with forEach
-const evenNumbers = numbers.forEach(number) 
+// const evenNumbers = numbers.filter((number) => number % 2 === 0);
+// const evenNumbers = numbers.filter((number) => number % 2 === 0);
 
-console.log(evenNumbers);
+// console.log(evenNumbers);
+
+const companies = [
+  { name: "Company One", category: "Finance", start: 2001, end: 2004 },
+  { name: "Company Two", category: "retail", start: 2011, end: 2014 },
+  { name: "Company Three", category: "Auto", start: 1921, end: 2003 },
+  { name: "Company four", category: "retail", start: 1321, end: 2003 }
+];
+
+//get only retail companies
+// const getRetail = companies.filter((company) => company.category == "retail");
+
+// console.log(getRetail);
+
+// get companies that started in or after 1980 and ended in or before 2005
+const getCompany = companies.filter(
+  (company) => company.start >= 1980 && company.end <= 2005
+);
+// console.log(getCompany);
+
+//Get copmanies that lasted 10 years
+const last10Year = companies.filter(
+  (company) => company.end - company.start > 10
+);
+console.log(last10Year);
---
+++ Modern_JS/05-loops-iteration-array/09.array.map/script.js
@@ -0,0 +1,3 @@
+const numbers = [1, 2, 3, 4, 5];
+const doubleNumbers = numbers.map((number) => number * 2);
+console.log(doubleNumbers);
