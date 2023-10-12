456d272d983acbf5404863b95781f8ba20eb8732
Michael Lay <michaellay2689@gmail.com>
2023-10-11 22:33:46 -0400

    new commit

--- 10-fetch-and-asyncAwait/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-asyncAwait/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -4,6 +4,18 @@ const getToDos = () => {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
+      //1. loop through the array of object
+      data.forEach((todo) => {
+        //create a new element div
+        const div = document.createElement("div");
+        //append or push the todo.title text node to the div
+        div.appendChild(document.createTextNode(todo.title));
+        // div.textContent = todo.title; //can also do this
+        //get element with id of todo-list
+        const todoList = document.getElementById("todo-list");
+        //append the newly created div into the todoList
+        todoList.appendChild(div);
+      });
     });
 };
 
