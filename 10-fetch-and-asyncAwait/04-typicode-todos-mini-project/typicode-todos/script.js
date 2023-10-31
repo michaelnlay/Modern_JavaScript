2b54e36883ec843dafd6943b55baae171b94a86a
Michael Lay <michaellay2689@gmail.com>
2023-10-31 11:22:23 -0400

    new commit

--- 10-fetch-and-asyncAwait/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-asyncAwait/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -1,14 +1,17 @@
 const apiURL = "https://jsonplaceholder.typicode.com/todos";
+
+//Step 1. fetch the API
 const getToDos = () => {
   fetch(apiURL + "?_limit=5")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      //1. loop through the array of object
+      console.log(data); //put these data into the DOM in the todo-list ID
+      //Step 1A. loop through the array of object (each data) to add to the DOM
       data.forEach((todo) => addTodoToDOM(todo));
     });
 };
 
+// Step 1B. create a new element to display the todo data on the DOM
 const addTodoToDOM = (todo) => {
   //create a new element div
   const div = document.createElement("div");
@@ -16,9 +19,11 @@ const addTodoToDOM = (todo) => {
   div.appendChild(document.createTextNode(todo.title));
   // div.textContent = todo.title; //can also do this
 
+  //Step 1D.
   // Add custom attribute to each one that has the id, use setAttribute
   div.setAttribute("data-id", todo.id); //use data-id when dealing with custom attribute
 
+  //Step 1C.
   //change background if the todo is completed
   if (todo.completed) {
     div.classList.add("done");
